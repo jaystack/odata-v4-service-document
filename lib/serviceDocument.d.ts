0cd57ead1d7bd678ce2f5d27aed2960f94c259c3
slajher <slajher92@gmail.com>
2017-08-22 13:34:25 +0200

    Support type definitions

--- lib/serviceDocument.d.ts
+++
@@ -1,9 +0,0 @@
-import { Edm } from 'odata-v4-metadata';
-import { ServiceMetadata } from 'odata-v4-service-metadata';
-import { Request, Response, RequestHandler } from 'express';
-export declare class ServiceDocument extends ServiceMetadata {
-    constructor(edmx: Edm.Edmx, options?: Object);
-    document(format?: string): any;
-    process(edmx: Edm.Edmx, options?: Object): void;
-    requestHandler(format?: string): (req: Request, res: Response, next: RequestHandler) => void;
-}
